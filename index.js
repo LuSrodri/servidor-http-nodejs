d34b483039e0426263a326e5df5e5b24958b3684
Lucas Santos Rodrigues <lucassrodri08@gmail.com>
2023-09-20 15:56:50 -0300

    atualizando para usar a biblioteca Net em vez do http

--- index.js
+++ index.js
@@ -1,31 +1,74 @@
-const http = require('http');
-const url = require('url');
+const net = require('net');
 const fs = require('fs');
+const path = require('path');
+const mime = require('mime-types');
 
-// Servidor HTTP
-http.createServer(async (request, response) => {
-    // Parseando URL
-    const parsedUrl = url.parse(request.url);
+const BASE_DIR = process.argv[2] || __dirname + '/public';
+const PORT = process.argv[3] || 8080;
+const TIMEOUT = 20000;
 
-    // Verificando o método do request
-    if (request.method !== 'GET') {
-        response.statusCode = 501;
-        response.end('Você precisa usar o método HTTP GET');
-        return;
-    }
+function handleRequest(socket) {
+    const resume = {};
 
-    // Construindo caminho do arquivo
-    const pathname = `./${parsedUrl.pathname}`;
+    socket.setEncoding('utf-8');
 
-    // Lendo arquivo
-    fs.readFile(pathname, (error, file) => {
-        if (error) {
-            response.statusCode = 404;
-            response.end(`O arquivo ${pathname} não foi encontrado!`);
-            return;
+    socket.on('data', (data) => {
+        const request = data.trim().split(' ');
+        const method = request[0];
+        resume.method = method;
+        let filePath;
+        if (request[1] === '/' || request[1] === '') filePath = path.join(BASE_DIR, 'index.html');
+        else filePath = path.join(BASE_DIR, request[1]);
+        resume.filePath = request[1];
+        const contentType = mime.lookup(filePath);
+
+        if (method === 'GET') {
+            fs.readFile(filePath, (err, content) => {
+                if (err) {
+                    socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+                    resume.statusCode = 404;
+                    socket.end();
+                } else {
+                    socket.write('HTTP/1.1 200 OK\r\n');
+                    socket.write('Server: Lucas`s server\r\n');
+                    socket.write(`Content-Type: ${contentType}\r\n`);
+                    socket.write(`Content-Length: ${content.length}\r\n`);
+                    socket.write('\r\n');
+                    socket.write(content);
+                    resume.statusCode = 200;
+                    socket.end();
+                }
+            });
+        } else {
+            socket.write('HTTP/1.1 502 Bad Gateway\r\n\r\n');
+            resume.statusCode = 502;
+            socket.end();
         }
-        response.end(file);
     });
-}).listen(8000, () => {
-    console.log('Servidor rodando na porta 8000');
+
+    socket.on('end', () => {
+        console.log(`${Date.now()} - ${socket.remoteAddress} - ${resume.method} ${resume.filePath} - ${resume.statusCode}`);
+        fs.writeFile(path.join((process.argv[2] || __dirname), 'log.txt'), `${Date.now()} - ${socket.remoteAddress} - ${resume.method} ${resume.filePath} ${resume.statusCode}\n`, { flag: 'a' }, (err) => {
+            if (err) {
+                console.log(err);
+                throw err;
+            }
+        });
+    });
+}
+
+const server = net.createServer();
+
+server.on('connection', (socket) => {
+    socket.setTimeout(TIMEOUT);
+
+    socket.on('timeout', () => {
+        socket.end();
+    });
+
+    handleRequest(socket);
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
